f2442ba213d61dc2cb22c60954bdff97f16d51b2
UscMel <vikram@email.sc.edu>
2018-05-28 20:17:46 -0400

    simulator.py

---
+++ Simulator.py
@@ -0,0 +1,30 @@
+"""
+This code is run to show end product of using different systems to maximize gains using low risk- low reward and high risk-high reward stock. 
+
+Author: Vikram Melchizedec
+
+"""
+
+
+"""
+Input: Amount(Monetary) , preferred system, Iterations amount
+Output: Ending amount for x amount of trials
+"""
+def main():
+	
+"""
+Meant to simuate a low risk low reward situation
+"""
+def lolo(Amount):
+
+"""
+Meant to simuate high risk, high reward situation
+"""
+def hihi():
+
+
+def AllOnLow():
+
+def AllOnHigh():
+
+def MelSystemOne():
