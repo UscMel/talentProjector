b0028b4bda81b3acb6e1a5d9cc63a0d2d9e61413
UscMel <vikram@email.sc.edu>
2018-05-28 20:52:21 -0400

    It runs

--- Simulator.py
+++ Simulator.py
@@ -8,28 +8,27 @@ Author: Vikram Melchizedec
 import random
 
 
-Print("Hello, welcome to the Stock simulator")
-Print("Please enter starting capital $$$$$")
-"""
-Input: Amount(Monetary) , preferred system, Iterations amount
-Output: Ending amount for x amount of trials
-"""
+# Input: Amount(Monetary) , preferred system, Iterations amount
+# Output: Ending amount for x amount of trials
 def main():
+    print("Hello, welcome to the Stock simulator")
+    print("Please enter starting capital $$$$$")
 
-
-"""
-Meant to simuate a low risk low reward situation
-"""
+# low risk low reward simulator for one iteration
 def lolo(Amount):
+	return Amount
 
-"""
-Meant to simuate high risk, high reward situation
-"""
-def hihi():
-
+# high risk high reward simulator for one iteration
+def hihi(Amount):
+    return Amount
 
 def AllOnLow():
-
+	return print("function run")
 def AllOnHigh():
+	return print("function run")
 
 def MelSystemOne():
+	return print("function run")
+
+if __name__ == "__main__":
+    main()
