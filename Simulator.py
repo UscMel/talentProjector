3dff302373a5cc7aa72f53b636380b9e42717ec0
UscMel <vikram@email.sc.edu>
2018-05-30 21:58:09 -0400

    added the high risk high value method which spits of stock values over period of time

--- Simulator.py
+++ Simulator.py
@@ -6,29 +6,59 @@ Author: Vikram Melchizedec
 """
 
 import random
+import sys
+import csv
 
 
 # Input: Amount(Monetary) , preferred system, Iterations amount
 # Output: Ending amount for x amount of trials
 def main():
     print("Hello, welcome to the Stock simulator")
-    print("Please enter starting capital $$$$$")
+    Capital = float(input("Pleae enter your starting capital $$$: "))
+    Iterations = float(input("How many iterations would you like to run? "))
+    print("Starting capital $$$ ",  Capital,"$")
+    print("Iterations: ", Iterations)
+    #LowValues = loloValue()
+    HighValues = hihiValue(Iterations)
+    print(*HighValues, sep = "\n")
 
-# low risk low reward simulator for one iteration
-def lolo(Amount):
-	return Amount
+# low risk low reward simulator for all iterations
+def loloValue():
+	Value = 1
+	LowValue = list.append(Value)
+	return LowValue
 
-# high risk high reward simulator for one iteration
-def hihi(Amount):
-    return Amount
+# high risk high reward simulator for all iterations
+def hihiValue(n):
+	Value = 1
+	HighValue = list()
+	HighValue.append(Value)
+	i = 1
+	multiplier = [50,66,80,100,125,150,200]
+	#While loop to generate stock values for all iterations
+	while (i < n):
+	    randMult = random.choice(multiplier)
+	    randMult = randMult  / 100
+	    Value = Value * randMult
+	    HighValue.append(Value)
+	    i = i + 1
+	    #Safeguards from Extremes... Coming soon
+
+	
+	return HighValue
 
 def AllOnLow():
 	return print("function run")
+#returns value of investment/capital after x amount of iterations if user let it ride. 
 def AllOnHigh():
 	return print("function run")
 
-def MelSystemOne():
+#returns value of investmennt/capital if investor used the Mel system of investing.
+def MelSystemOne(Capital):
 	return print("function run")
+
+def StockBroker(amountIN, Iteration ):
+	return Value
 
 if __name__ == "__main__":
     main()
